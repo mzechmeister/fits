0abe4b66c50b1a4aa84fa8e4f0d9de8eb353cc59
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2022-12-27 10:40:38 +0100

    feat: Plot fits table columns with csvplotter.
    This is a quick hack using iframe. Only few modifications are needed. csv_plotter.htm is used without any modifications. Only dsv.js now is rewritten as a wrapper to the fits reader. The fits file needs to be reread. Thus quite slow. A more intergated solution will make maintance more difficult (many work arounds, code duplication, ...).

---
+++ dsv.js
@@ -0,0 +1,30 @@
+document.write('<script src="fits.js"></script>');
+
+async function dsv(file, ...args) {
+    if (typeof file == "string") {
+        // nothing needed if file is instanceof File
+        [file, ext] = file.split(/[\[\]]/)
+        var hdulist = await readfits(file);
+    }
+    var responseText = await fitsdata(hdulist, ext)
+    if (!file.ok) console.log(file.statusText, "("+file.url+")")
+    if (responseText[0]) {
+        responseText.__proto__._url = file //.name || file.url
+        text_to_table(responseText, ...args)
+    }
+}
+
+function auto_colnames(data) {
+    // check for a header row (no numbers) and convert it to column names
+    colnames = data.map(d => d.name)
+    data._colnames = colnames
+    data.unshift([...Array(data[0].length).keys()])  // prepend a column 0 with row numbers
+    console.log(colnames)
+}
+
+function text_to_table(dataorg, func, delim) {
+    data = dataorg   // global variable
+    auto_colnames(data)
+    data._basename = data._url.split('/').pop()
+    func(data)
+}
